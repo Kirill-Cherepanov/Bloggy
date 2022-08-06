70e0c08cafbec4b9925c674ba22b52648a34bed4
Cherepanov Kirill <cherkirr@gmail.com>
2022-08-06 18:02:35 +0300

    feat: add styling to homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage/Homepage';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        {/* <Route path="/posts">
+          <Homepage />
+        </Route> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
