88b26f1dfae75dd59c92d07cd63f9a1891c7a64d
Cherepanov Kirill <cherkirr@gmail.com>
2022-08-07 13:28:57 +0300

    feat: add register page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
 import Topbar from './components/Topbar/Topbar';
+import Register from './pages/Register/Register';
 
 function App() {
+  const currentUser = true;
   return (
     <Router>
       <Topbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/posts" element={<Homepage />} />
+        <Route path="/posts" element={<Homepage />} />
+        <Route
+          path="/register"
+          element={currentUser ? <Homepage /> : <Register />}
+        />
+        {/* <Route path="/login" element={currentUser ? <Homepage /> : <Login />} />
+        <Route path="/post/:id" element={<Single />} />
+        <Route path="/write" {currentUser ? <Write /> : <Login />} />
+        <Route path="/settings" element={currentUser ? <Settings /> : <Login />} /> */}
       </Routes>
     </Router>
   );
---
+++ client/src/pages/Register/Register.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Register.css';
+
+export default function Register() {
+  return (
+    <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label>Username</label>
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Enter your username..."
+        />
+        <label>Email</label>
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Enter your email..."
+        />
+        <label>Password</label>
+        <input
+          className="registerInput"
+          type="password"
+          placeholder="Enter your password..."
+        />
+        <button className="registerButton">Register</button>
+      </form>
+      <button className="registerLoginButton">Login</button>
+    </div>
+  );
+}
