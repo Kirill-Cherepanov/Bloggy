64ae9a7524e2188b9a67ed4f5b8ee110772abc37
Cherepanov Kirill <cherkirr@gmail.com>
2022-08-07 13:20:34 +0300

    feat: add topbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
+import Topbar from './components/Topbar/Topbar';
 
 function App() {
   return (
     <Router>
+      <Topbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        {/* <Route path="/posts">
-          <Homepage />
-        </Route> */}
+        <Route path="/posts" element={<Homepage />} />
       </Routes>
     </Router>
   );
