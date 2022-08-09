d94789ce186ac827d449782ff94356c7abcea7c2
KissMyUSSR <cherkirr@gmail.com>
2022-08-09 21:58:20 +0300

    feat: add login

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,25 +1,55 @@
+import axios from 'axios';
+import React, { useContext, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { Context } from '../../context/Context';
 import './Login.css';
 
 export default function Login() {
+  const userRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const { dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: 'LOGIN_START' });
+    try {
+      const res = await axios.post('/auth/login', {
+        username: userRef.current!.value,
+        password: passwordRef.current!.value
+      });
+      dispatch({ type: 'LOGIN_SUCCESS', payload: res.data });
+    } catch (err) {
+      dispatch({ type: 'LOGIN_FAILURE' });
+    }
+  };
+
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label>Email</label>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
         <input
-          className="loginInput"
           type="text"
-          placeholder="Enter your email..."
+          className="loginInput"
+          placeholder="Enter your username..."
+          ref={userRef}
         />
         <label>Password</label>
         <input
-          className="loginInput"
           type="password"
+          className="loginInput"
           placeholder="Enter your password..."
+          ref={passwordRef}
         />
-        <button className="loginButton">Login</button>
+        <button className="loginButton" type="submit" disabled={isFetching}>
+          Login
+        </button>
       </form>
-      <button className="loginRegisterButton">Register</button>
+      <button className="loginRegisterButton">
+        <Link className="link" to="/register">
+          Register
+        </Link>
+      </button>
     </div>
   );
 }
