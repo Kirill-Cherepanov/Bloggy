c708f449382c0e9c2d17049db02cc17ec733eace
KissMyUSSR <cherkirr@gmail.com>
2022-08-09 23:04:06 +0300

    feat: add write

--- client/src/pages/Write/Write.tsx
+++ client/src/pages/Write/Write.tsx
@@ -1,32 +1,79 @@
 import './Write.css';
+import axios from 'axios';
+import { Context } from '../../context/Context';
+import React, { useContext, useState } from 'react';
 
 export default function Write() {
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [file, setFile] = useState<File | null>(null);
+  const { user } = useContext(Context);
+
+  // TODO: make it redirect to /login
+  if (user === null) {
+    throw Error(
+      "Can't change user options parameters since user is undefined!"
+    );
+  }
+
+  const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newPost = {
+      username: user.username,
+      title,
+      desc
+    };
+
+    // TODO: fix this shit x2
+    // if (file) {
+    //   const data = new FormData();
+    //   const filename = Date.now() + file.name;
+    //   data.append('name', filename);
+    //   data.append('file', file);
+    //   newPost.photo = filename;
+    //   try {
+    //     await axios.post('/upload', data);
+    //   } catch (err) {}
+    // }
+
+    try {
+      const res = await axios.post('/posts', newPost);
+      window.location.replace('/post/' + res.data._id);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="write">
-      <img
-        className="writeImg"
-        src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt=""
-      />
-      <form className="writeForm">
+      {file && (
+        <img className="writeImg" src={URL.createObjectURL(file)} alt="" />
+      )}
+      <form className="writeForm" onSubmit={handleSubmit}>
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
             <i className="writeIcon fas fa-plus"></i>
           </label>
-          <input id="fileInput" type="file" style={{ display: 'none' }} />
           <input
-            className="writeInput"
-            placeholder="Title"
+            type="file"
+            id="fileInput"
+            style={{ display: 'none' }}
+            onChange={(e) => setFile(e.target.files?.[0] || null)}
+          />
+          <input
             type="text"
+            placeholder="Title"
+            className="writeInput"
             autoFocus={true}
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="writeFormGroup">
           <textarea
+            placeholder="Tell your story"
             className="writeInput writeText"
-            placeholder="Tell your story..."
-            autoFocus={true}
-          />
+            onChange={(e) => setDesc(e.target.value)}
+          ></textarea>
         </div>
         <button className="writeSubmit" type="submit">
           Publish
