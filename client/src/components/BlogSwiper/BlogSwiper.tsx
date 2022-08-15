56da9d9e7580719ad070708dfafa8cd91f8215e8
KissMyUSSR <cherkirr@gmail.com>
2022-08-16 00:46:56 +0300

    feat: make BlogSwiper card fully responsive

--- client/src/components/BlogSwiper/BlogSwiper.tsx
+++ client/src/components/BlogSwiper/BlogSwiper.tsx
@@ -4,18 +4,36 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/a11y';
 import 'swiper/css/navigation';
+import { useEffect, useState } from 'react';
 
 type Props = { mockImage: string };
 
+const getSlidesAmount = () => {
+  if (window.innerWidth >= 1200) return 4;
+  if (window.innerWidth >= 800) return 3;
+  if (window.innerWidth >= 600) return 2;
+  return 1;
+};
+
 export default function BlogSwiper({ mockImage }: Props) {
+  const [slidesAmount, setSlidesAmount] = useState(getSlidesAmount());
+  useEffect(() => {
+    const setSlides = () => setSlidesAmount(getSlidesAmount());
+    window.addEventListener('resize', setSlides);
+
+    return () => {
+      window.removeEventListener('resize', setSlides);
+    };
+  }, []);
+
   return (
     <div className="w-full px-10 flex items-center">
       <Icon
         type="angle"
-        className="BLOG_SWIPER_PREV_ELEMENT text-stone-600 basis-10 shrink-0"
+        className="BLOG_SWIPER_PREV_ELEMENT text-stone-600 shrink-0 basis-8 xs:basis-10"
       />
       <Swiper
-        slidesPerView={3}
+        slidesPerView={slidesAmount}
         modules={[Navigation, A11y]}
         navigation={{
           nextEl: '.BLOG_SWIPER_NEXT_ELEMENT',
@@ -29,20 +47,23 @@ export default function BlogSwiper({ mockImage }: Props) {
           .fill(0)
           .map((v, i) => (
             <SwiperSlide key={i} className="py-4">
-              <div className="mx-5 px-4 py-2 bg-accent-50 shadow-lg hover:scale-105">
+              <div className="px-4 py-2 bg-accent-50 shadow-lg hover:scale-105 xs:mx-5">
                 <img
                   src={mockImage}
                   alt="Blogger"
                   className="mb-4 rounded-full aspect-square object-cover select-none cursor-pointer"
                 />
-                <h4 className="font-bold text-center text-ellipsis cursor-pointer overflow-hidden hover:underline mb-1">
+                <h4 className="font-bold text-lg text-center text-ellipsis cursor-pointer overflow-hidden hover:underline mb-1">
                   Bloggercoolnameandstuffffafafafafafaff
                 </h4>
                 <div className="flex gap-4 mb-2 overflow-hidden flex-wrap h-7">
                   {Array(5)
                     .fill(0)
                     .map((v, i) => (
-                      <span className="border border-dark inline-block h-full px-1 hover:border-accent-600 hover:text-accent-600 hover:shadow-md hover:shadow-accent-400 cursor-pointer">
+                      <span
+                        key={i}
+                        className="border border-dark inline-block h-full px-1 hover:border-accent-600 hover:text-accent-600 hover:shadow-md hover:shadow-accent-400 cursor-pointer"
+                      >
                         Music
                       </span>
                     ))}
@@ -60,7 +81,7 @@ export default function BlogSwiper({ mockImage }: Props) {
       </Swiper>
       <Icon
         type="angle"
-        className="BLOG_SWIPER_NEXT_ELEMENT text-stone-600 rotate-180 basis-10 shrink-0"
+        className="BLOG_SWIPER_NEXT_ELEMENT text-stone-600 rotate-180 shrink-0 basis-8 xs:basis-10"
       />
     </div>
   );
