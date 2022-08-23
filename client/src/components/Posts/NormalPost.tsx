43b13c6d62e081e2d6cfe4d1118442e6fedc0ccf
KissMyUSSR <cherkirr@gmail.com>
2022-08-23 17:55:36 +0300

    feat: add normal post and finish layout and basic styling of blog page

--- client/src/components/Posts/NormalPost.tsx
+++ client/src/components/Posts/NormalPost.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import formatDate from '../../utility/formatDate';
+import Icon from '../Icon/Icon';
 
-type Props = {};
-
-export default function NormalPost({}: Props) {
-  return <div>NormalPost</div>;
+export default function NormalPost({
+  title,
+  text,
+  image,
+  likes,
+  authorName,
+  createdAt
+}: Post) {
+  return (
+    <li className="lg:bg-accent-50 py-4 px-6">
+      <h3 className="font-display font-semibold text-xl">{title}</h3>
+      <p className="my-2 line-clamp-4">{text}</p>
+      <img src={image} alt="post" className="mb-2" />
+      <div className="flex justify-between items-center">
+        <div className="font-semibold flex items-center cursor-pointer group">
+          <span className="relative w-5 h-5 mr-2">
+            <Icon
+              type="heart-outline"
+              className="absolute top-0 left-0 w-full text-secondary-800 group-hover:text-transparent"
+            />
+            <Icon
+              type="heart"
+              className="absolute top-0 left-0 w-full fill-transparent group-hover:fill-red-400"
+            />
+          </span>
+          {likes}
+        </div>
+        <div className="mt-auto text-text-600 flex flex-col xs:block">
+          <span className="hidden xs:inline">By </span>
+          <span
+            className={
+              'cursor-pointer font-bold hover:underline text-accent-600'
+            }
+          >
+            {authorName}
+          </span>
+          <span className="font-light text-sm md:text-base">
+            <span className="hidden xs:inline mx-1"> | </span>
+            {formatDate(createdAt)}
+          </span>
+        </div>
+      </div>
+    </li>
+  );
 }
