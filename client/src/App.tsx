d29857c317b93f72fd737354af12f7afc67666b8
KissMyUSSR <cherkirr@gmail.com>
2022-08-25 20:32:17 +0300

    feat: add extensive planning for sign up and login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,12 +8,12 @@ import Write from './pages/Write/Write';
 // import Settings from './pages/Settings/Settings';
 import Blog from './pages/Blog/Blog';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useContext } from 'react';
-import { Context } from './context/Context';
+// import { useContext } from 'react';
+// import { Context } from './context/Context';
 import Catalog from './pages/Catalog/Catalog';
 
 export default function App() {
-  const { user } = useContext(Context);
+  // const { user } = useContext(Context);
   return (
     <Router>
       <Topbar />
@@ -21,10 +21,10 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/blog/:name" element={<Blog />} />
         <Route path="/catalog" element={<Catalog />} />
-        <Route path="/sign-up" element={user ? <Homepage /> : <Register />} />
-        <Route path="/login" element={user ? <Homepage /> : <Login />} />
+        <Route path="/sign-up" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/post/:id" element={<Post />} />
-        <Route path="/write" element={user ? <Write /> : <Login />} />
+        <Route path="/write" element={<Write />} />
         {/* <Route path="/settings" element={user ? <Settings /> : <Login />} /> */}
       </Routes>
       <Footer />
