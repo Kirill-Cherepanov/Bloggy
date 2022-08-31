8dc6e7768b36812b2df955bc3a336d50cf0d5c74
KissMyUSSR <cherkirr@gmail.com>
2022-08-31 21:13:11 +0300

    refactor: rewrite SearchDb in OOP

--- api/src/utility/SearchDb.ts
+++ api/src/utility/SearchDb.ts
@@ -1,17 +1,18 @@
 import Post from '../models/Post';
+import User from '../models/User';
 
-const searchParams_ = {
-  type: ['posts', 'blogs'],
-  search: ['categories', 'title', 'both'],
-  sort: ['popular', 'new', 'best match'],
-  time: ['week', 'month', 'year', 'all'],
-  page: Number,
-};
+// const searchParams_ = {
+//   type: ['posts', 'blogs'],
+//   search: ['categories', 'title', 'both'],
+//   sort: ['popular', 'new', 'best match'],
+//   time: ['week', 'month', 'year', 'all'],
+//   page: Number,
+// };
 
-const newQueryUrl =
-  'http://localhost:5000/api/posts?q=post&type=posts&search=categories&sort=new&time=all&authorName=test';
-const oldQueryUrl =
-  'http://localhost:5000/api/posts?category=Science&username=KissMe';
+// const oldQueryUrl =
+//   'http://localhost:5000/api/posts?category=Science&username=KissMe';
+// const newQueryUrl =
+//   'http://localhost:5000/api/posts?q=post&type=posts&search=categories&sort=new&time=all&authorName=test';
 
 export type SearchParams = {
   q: string;
@@ -24,75 +25,79 @@ export type SearchParams = {
 
 type SearchQuery = any;
 
-const POSTS_PER_PAGE = 10;
+export class SearchDb {
+  protected DEFAULT_PARAMS: SearchParams = {
+    q: '',
+    type: 'posts',
+    search: 'both',
+    sort: 'new',
+    time: 'all',
+    page: 1,
+  };
+  protected POSTS_PER_PAGE = 10;
 
-const DEFAULT_PARAMS: SearchParams = {
-  q: '',
-  type: 'posts',
-  search: 'both',
-  sort: 'new',
-  time: 'all',
-  page: 1,
-};
+  protected params: SearchParams;
 
-export default async function searchDb(requestParams: Partial<SearchParams>) {
-  const params: SearchParams = Object.assign(DEFAULT_PARAMS, requestParams);
+  constructor(requestParams: Partial<SearchParams>) {
+    this.params = Object.assign(this.DEFAULT_PARAMS, requestParams);
+  }
 
-  // .type
-  if (params.type === 'posts') return searchPosts(params);
-  return searchBlogs(params);
+  protected getSortQuery(sort: string) {
+    if (sort === 'new') return { createdAt: 1 };
+    if (sort === 'popular') return { likes: 1 };
+    else throw Error('Incorrect "Sort by" value');
+  }
+
+  protected getSearchQuery(search: string, query: string) {
+    // CHECK WHAT HAPPENS WHEN params.q === ''
+
+    const searchSwitch = {
+      title: this.getTitleQuery(query),
+      categories: this.getCategoriesQuery(query),
+      both: {
+        $or: [this.getTitleQuery(query), this.getCategoriesQuery(query)],
+      },
+    };
+
+    if (!(search in searchSwitch)) throw Error('Incorrect "Search by" value');
+    return searchSwitch[search as keyof typeof searchSwitch];
+  }
+
+  protected getTitleQuery(query: string) {
+    return {
+      $title: {
+        $search: query,
+        $caseSensitive: false,
+      },
+    };
+  }
+
+  protected getCategoriesQuery(query: string) {
+    return {
+      categories: {
+        $in: query.toLowerCase().split(' '),
+      },
+    };
+  }
 }
 
-async function searchPosts(params: SearchParams) {
-  // let query: SearchQuery = {};
-
-  // .search
-  const query: SearchQuery = {
-    ...getSearchQuery(params.search, params.q),
-    ...getSortQuery(params.sort),
-  };
-
-  // .sort
-  if (params.sort === 'new') {
-    query.$sort = { createdAt: 1 };
+export class SearchPosts extends SearchDb {
+  constructor(requestParams: Partial<SearchParams>) {
+    super(requestParams);
   }
-  if (params.sort === 'popular') {
-    query.$sort = { likes: 1 };
+
+  public getPosts() {
+    const query: SearchQuery = {
+      ...this.getSearchQuery(this.params.search, this.params.q),
+      ...this.getSortQuery(this.params.sort),
+    };
   }
 }
 
-const getSortQuery = (sort: string) => {
-  if (sort === 'new') return { createdAt: 1 };
-  if (sort === 'popular') return { likes: 1 };
-  else throw Error('Incorrect "Sort by" value');
-};
-
-const getSearchQuery = (search: string, query: string) => {
-  // CHECK WHAT HAPPENS WHEN params.q === ''
-
-  const searchSwitch = {
-    title: getTitleQuery(query),
-    categories: getCategoriesQuery(query),
-    both: { $or: [getTitleQuery(query), getCategoriesQuery(query)] },
-  };
-
-  if (!(search in searchSwitch)) throw Error('Incorrect "Search by" value');
-  return searchSwitch[search as keyof typeof searchSwitch];
-};
-
-const getTitleQuery = (query: string) => ({
-  $title: {
-    $search: query,
-    $caseSensitive: false,
-  },
-});
-
-const getCategoriesQuery = (query: string) => ({
-  categories: {
-    $in: query.toLowerCase().split(' '),
-  },
-});
-
-async function searchBlogs(params: SearchParams) {}
+export class SearchBlogs extends SearchDb {
+  constructor(requestParams: Partial<SearchParams>) {
+    super(requestParams);
+  }
+}
 
 export async function searchBlogPosts() {}
