0ea687f65ff400c50a9fb97ade3517416b12ea46
KissMyUSSR <cherkirr@gmail.com>
2022-09-17 10:28:34 +0300

    feat: add a cool spinner

--- client/src/components/Elements/Spinner.tsx
+++ client/src/components/Elements/Spinner.tsx
@@ -1,15 +1,15 @@
 import clsx from 'clsx';
 
 const sizes = {
-  sm: 'h-4 w-4',
-  md: 'h-8 w-8',
-  lg: 'h-16 w-16',
-  xl: 'h-24 w-24',
+  sm: ['h-9 w-9', 'h-3 w-3 border-2'],
+  md: ['h-15 w-15', 'h-5 w-5 border-2'],
+  lg: ['h-24 w-24', 'h-8 w-8 border-[3px]'],
 };
 
 const variants = {
-  light: 'text-white',
-  primary: 'text-accent-600',
+  light: ['border-main', 'bg-main'],
+  accent: ['border-accent-900', 'bg-accent-900'],
+  dark: ['border-secondary-800', 'bg-secondary-800'],
 };
 
 export type SpinnerProps = {
@@ -19,39 +19,31 @@ export type SpinnerProps = {
 };
 
 export const Spinner = ({
-  size = 'md',
-  variant = 'primary',
+  size = 'lg',
+  variant = 'dark',
   className = '',
 }: SpinnerProps) => {
+  const currentVariant = variants[variant];
+  const currentSize = sizes[size];
+
   return (
     <>
-      <svg
+      <div
         className={clsx(
-          'animate-spin',
-          sizes[size],
-          variants[variant],
+          'spinner',
+          currentVariant[0],
+          currentSize[0],
           className
         )}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        data-testid="loading"
       >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          strokeWidth="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-      <span className="sr-only">Loading</span>
+        <div className={clsx('blob top', currentSize[1])}></div>
+        <div className={clsx('blob bottom', currentSize[1])}></div>
+        <div className={clsx('blob left', currentSize[1])}></div>
+
+        <div
+          className={clsx('blob move-blob', currentSize[1], currentVariant[1])}
+        ></div>
+      </div>
     </>
   );
 };
