4699741d49194e5c2ba417ecde5b4366ef06822e
KissMyUSSR <cherkirr@gmail.com>
2022-10-24 12:38:40 +0300

    deploy: transfer deploy url from .env into config

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,5 +1,6 @@
+const DEPLOY_API_URL = 'https://bloggy-api.onrender.com/api';
 export const API_URL =
-  process.env.NODE_ENV === 'production' ? process.env.API_URL! : '/api';
+  process.env.NODE_ENV === 'production' ? DEPLOY_API_URL : '/api';
 
 export const PROFILE_PICS_LOCATION = API_URL + '/images/profilePics/';
 export const POST_IMGS_LOCATION = API_URL + '/images/postImgs/';
