fd3485eee8036e3726bc91664777ddfee0bc813b
KissMyUSSR <cherkirr@gmail.com>
2022-10-24 12:32:26 +0300

    deploy: debug environment variables on client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,11 @@
+import { API_URL } from 'config';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
+console.log(process.env.NODE_ENV);
+console.log(process.env.API_URL);
+console.log(API_URL);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
